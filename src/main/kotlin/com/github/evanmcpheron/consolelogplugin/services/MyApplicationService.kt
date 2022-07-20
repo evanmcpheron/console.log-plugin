14806ed31e94358d2a9744e3fa613c303841fff2
GitHub Action <action@github.com>
2022-07-20 14:45:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/evanmcpheron/consolelogplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.evanmcpheron.consolelogplugin.services
+
+import com.github.evanmcpheron.consolelogplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
